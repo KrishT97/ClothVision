14c6cf66ef47df2a1d47b107a2c91bd881865d17
Davoestacogido <davidcruzsanchez111@gmail.com>
2023-11-19 19:46:33 +0000

    dataloaders done, time to model

--- test.py
+++ test.py
@@ -1,17 +1,7 @@
-import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
 
-# Sample data
-data = [('cosa', 2), ('cosa', 3)]
 
-# Convert list of tuples to DataFrame
-df = pd.DataFrame(data, columns=['Category', 'Count'])
-
-# Create an instance of OneHotEncoder
-encoder = OneHotEncoder(sparse=False)
-
-# Fit and transform the DataFrame
-encoded_array = encoder.fit_transform(df[['Category', 'Count']])
-
-# Print the result
-print(encoded_array)
+from torchvision import transforms
+if transforms.RandomHorizontalFlip():
+    print("tes")
+else:
+    print("no none")
